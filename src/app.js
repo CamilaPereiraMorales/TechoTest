cd9e747105bee7727b8798d2d7f267adff19a6c7
Camili <camipereira2111@gmail.com>
2019-03-26 18:06:14 -0300

    maqueta y estilo en mobile y desktop avanzada

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-let url = (`http://proyectos.chile.techo.org/2019/php/select_campamentos.php`)
+// let url = (`http://proyectos.chile.techo.org/2019/php/select_campamentos.php`)
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
         
-    })
+//     })
