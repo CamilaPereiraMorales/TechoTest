ebe568a87763f0ac4323b17d2c81cf897f412538
Camili <camipereira2111@gmail.com>
2019-03-26 13:47:00 -0300

    Fetch+HTML base

---
+++ src/app.js
@@ -0,0 +1,8 @@
+let url = (`http://proyectos.chile.techo.org/2019/php/select_campamentos.php`)
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        
+    })
