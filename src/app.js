71d7721b883c024f1f98ddee6ab65250e3ee2377
Camili <camipereira2111@gmail.com>
2019-03-28 10:22:03 -0300

    console con los datos de la data que necesito pintar

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
-// let url = (`http://proyectos.chile.techo.org/2019/php/select_campamentos.php`)
+let url = (`http://proyectos.chile.techo.org/2019/php/select_campamentos.php`)
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+       regionCampamento = []
+       campamento = []
+       for(let i=0; i<data.length; i++){
+
+           ubicacionCampamento=data[i][0]
+           regionCampamento.push(ubicacionCampamento)
+
+           nombreCampamento=data[i][2]
+           campamento.push(nombreCampamento)
+
         
-//     })
+           console.log(regionCampamento[i] , campamento[i]);
+       }
+       
+      //console.log(nombreCampamento);
+      // console.log(ubicacionCampamento + nombreCampamento);
+      //console.log(campamento);
+      
+      
+       
+      
+       
+   
+       
+
+
+       
+       
+       
+       
+
+
+       
+       
+       
+       
+        
+    })
