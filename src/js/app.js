d9fa49b4d3b32dde58a51e25f683fa401af6cfab
Camili <camipereira2111@gmail.com>
2019-03-29 00:01:21 -0300

    todos los html listos, redireccionamientos ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
     function tipoProyecto() {
         location="./tipoProyecto.html";
      }
+
+     function escogerCampamento(){
+         location="./index.html";
+     }
+
+     function acercaProyecto() {
+         location="./acercaProyecto.html"
+     }
+     function gracias(){
+         location="./gracias.html"
+     }
